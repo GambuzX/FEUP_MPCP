df4af853f6ca9811d213a7869022124cd7a3e7fd
Mário Gil <mg_mesquita@hotmail.com>
2018-03-23 12:23:17 +0000

    Exercises 13 to 16

--- Exercises/exe16_a.asm
+++ Exercises/exe16_a.asm
@@ -1,17 +1,22 @@
 include mpcp.inc
 
 .data
-val1 WORD 4
-val2 WORD 3
 
 .code
 main PROC C
-xor eax,eax
-mov ax,val1
-mul val2
-sal dx,16
-or eax,edx
 
+cmp cl, ah
+jg INC
+
+cmp al, ah
+jle fim
+cmp bl, bh
+jle fims
+
+INC: 
+inc ecx
+
+fim:
 invoke ExitProcess,0
 
 main ENDP
