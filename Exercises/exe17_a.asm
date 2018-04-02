02171b3aeb6ae03e84b20b48401a4b0e04e1b3d5
Mário Gil <mg_mesquita@hotmail.com>
2018-04-02 16:21:16 +0100

    17 a

--- Exercises/exe17_a.asm
+++ Exercises/exe17_a.asm
@@ -1,33 +1,35 @@
 include mpcp.inc
 
-.data
+	.data
+sequence DWORD 1,2,3,4,5,6,12
+msg BYTE "Median value = %d", 13,10,0
 
-sequence DWORD 1,2,3,4,5,6
-
-.code
+	.code
 main PROC C
+	mov eax,0
+	mov ecx, LENGTHOF sequence
+	mov edx, OFFSET sequence
 
-mov eax,0
-mov ecx, LENGTHOF sequence
-mov edx, OFFSET sequence
+	;;Ciclo para somar os valores
+	ciclo: 
+	mov ebx, [edx]
+	add eax, ebx
+	add edx, TYPE sequence
+	loop ciclo
 
-;;Ciclo para somar os valores
-ciclo: 
-mov ebx, [edx]
-add eax, ebx
-add edx, TYPE sequence
-loop ciclo
+	mov ecx, LENGTHOF sequence
+	mov edx, 0
+	div ecx
 
-mov ecx, LENGTHOF sequence
-div ecx
+	sal edx,1 ;multiplico resto por 2
+	cmp edx,ecx ;comparo o dobro do resto com o divisor
+	jnge fim ;se nao for maior ou igual, nao e preciso incrementar
+	inc eax 
 
-sal edx,1 ;multiplico resto por 2
-cmp edx,ecx ;comparo o dobro do resto com o divisor
-jnge fim ;se nao for maior ou igual, nao e preciso incrementar
-inc eax 
-
-fim:
-invoke ExitProcess,0
+	fim:
+	invoke printf, offset msg, eax
+	invoke _getch
+	invoke ExitProcess,0
 
 main ENDP
 
