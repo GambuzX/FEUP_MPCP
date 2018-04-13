9700ebe8a1759a039eafe54bab3b736ee9469242
Mário Gil <mg_mesquita@hotmail.com>
2018-04-13 11:35:59 +0100

    Subroutines analysis

---
+++ Exercises/exe4_11.asm
@@ -0,0 +1,17 @@
+include mpcp.inc
+
+.data
+
+.code
+
+ROTINA PROC C uses EDI A:DWORD , B: DWORD
+ret
+ROTINA ENDP
+
+
+main PROC C
+	invoke ROTINA, eax, ebx
+	invoke ExitProcess, 0
+main ENDP
+
+end
