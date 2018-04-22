8aa462f8259ed3327fabe49aefa204dfec4e5578
Mário Gil <mg_mesquita@hotmail.com>
2018-04-22 10:56:58 +0100

    [17/18 2ºtest] Calculating parking lotation at a given time

---
+++ Exercises/test2-17-18.asm
@@ -0,0 +1,53 @@
+include mpcp.inc
+
+.data
+inP WORD 0,0,0,0,1,1,2,3,4,5,6,7,7,8
+outP WORD 1,1,1,2,2,2,3,4,5,6,6,6,8,9,10
+msg BYTE "%d",0,10,13
+
+.code
+
+nviaturas PROTO tin: PTR WORD, tout: PTR WORD, N: WORD, M: WORD
+
+main PROC C
+	invoke nviaturas, OFFSET inP, OFFSET outP, LENGTHOF inP, 1
+	invoke printf, OFFSET msg, eax
+	invoke _getch
+	invoke ExitProcess,0
+main ENDP
+
+nviaturas PROC tin: PTR WORD, tout: PTR WORD, N: WORD, M: WORD	
+	LOCAL count: DWORD
+	mov eax, 0
+	mov count, eax
+	xor ecx, ecx
+	mov cx, N
+	mov ebx, tin
+
+	.WHILE ecx > 0
+		mov dx, WORD PTR [ebx]
+		.IF M > dx
+			inc count
+		.ENDIF
+		add ebx, 2
+		dec ecx
+	.ENDW
+
+	xor ecx, ecx
+	mov cx, N
+	mov ebx, tout
+
+	.WHILE ecx > 0
+		mov dx, WORD PTR [ebx]
+		.IF M > dx
+			dec count
+		.ENDIF
+		add ebx, 2
+		dec ecx
+	.ENDW
+
+	mov eax, count
+	ret
+nviaturas ENDP
+
+end
