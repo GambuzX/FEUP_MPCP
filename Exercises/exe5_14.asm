c8cafef627f3fdd2b5127b4d4c9c240ec580cf15
Mário Gil <mg_mesquita@hotmail.com>
2018-04-27 12:51:52 +0100

    Multiplying a sequence by a value

--- Exercises/exe5_14.asm
+++ Exercises/exe5_14.asm
@@ -2,13 +2,25 @@ include mpcp.inc
 .xmm
 
 .data
+msg BYTE "Value = %f", 13, 10 ,0
 
+sequence REAL4 12.0, 3.4, 5.6, 9.1, 14.2
+alpha REAL4 0.5
+
+testVal REAL8 ?
 
 .code
 scaleSeq PROTO seq:PTR REAL4, N:DWORD, alfa:REAL4
 
 main PROC C
+	invoke scaleSeq, OFFSET sequence, LENGTHOF sequence, alpha
 
+	cvtss2sd xmm0, sequence
+	movsd testVal, xmm0
+	invoke printf, OFFSET msg, testVal
+
+	invoke _getch
+	invoke ExitProcess,0
 main ENDP
 
 scaleSeq PROC seq:PTR REAL4, N:DWORD, alfa:REAL4
@@ -18,8 +30,9 @@ scaleSeq PROC seq:PTR REAL4, N:DWORD, alfa:REAL4
 	.WHILE ecx > 0
 		movss xmm0, REAL4 PTR [esi]
 		mulss xmm0, alfa
+		movss REAL4 PTR [esi], xmm0
 
-
+		add esi, TYPE REAL4
 		dec ecx
 	.ENDW
 	ret
