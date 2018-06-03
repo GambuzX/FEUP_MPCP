471ae07d9236c07dd72240fccffe1dbeb442727e
Mário Gil <mg_mesquita@hotmail.com>
2018-06-03 10:52:03 +0100

    Finished 5_18

--- exe5_18.asm
+++ exe5_18.asm
@@ -16,6 +16,7 @@ conta_intervalo PROC pt:PTR REAL4, N:dword, limA:REAL4, limB:REAL4
 	mov edx, pt
 	mov ecx, N
 	shr ecx, 2
+	xor eax, eax
 
 	movss xmm1, limA
 	shufps xmm1, xmm1 0
@@ -30,8 +31,21 @@ conta_intervalo PROC pt:PTR REAL4, N:dword, limA:REAL4, limB:REAL4
 		cmpps xmm3, xmm2, 2 ;limite inferior
 
 		andps xmm0, xmm3 ;valores dentro dos limites ficam 11111...1111
+		xor ebx,ebx
 		movmskps ebx, xmm0
 
+		push ecx
+		mov ecx, 4
+		.WHILE ecx > 0
+			rcr ebx, 1
+			jnc next
+			inc eax
+
+			next:
+			dec ecx	
+		.ENDW
+
+		pop ecx
 		dec ecx
 		add edx, 16
 	.ENDW
